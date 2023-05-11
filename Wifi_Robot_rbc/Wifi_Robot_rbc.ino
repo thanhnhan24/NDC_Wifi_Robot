16c329279b008b3c5ba40efba8dc2a439b255f35
thanhnhan24 <121763605+thanhnhan24@users.noreply.github.com>
2023-05-11 14:23:15 +0700

    Add files via upload

---
+++ Wifi_Robot_rbc/Wifi_Robot_rbc.ino
@@ -0,0 +1,152 @@
+/* https://arduino.esp8266.com/stable/package_esp8266com_index.json
+   ESP8266 as Web Server using WiFi Access Point (AP) mode
+   Connect to AP "Robot Wifi", password = "87654321"
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <Servo.h>
+
+//SSID and Password to your ESP Access Point
+const char* ssid = "NDC Robotcon Manaual-bot";
+const char* password = "NDC123456789";
+
+#define ENA   4     // Enable/speed motors Right    GPIO4(D2)
+#define IN_1  0     // L298N in1 motors Right       GPIO0(D3)
+#define IN_2  2     // L298N in2 motors Right       GPIO2(D4)
+#define IN_3  14    // L298N in3 motors Left        GPIO12(D5)
+#define IN_4  12    // L298N in4 motors Left        GPIO13(D6)
+#define ENB   13    // Enable/speed motors Left     GPIO15(D7)
+
+
+
+String command;             //String to store app command state.
+int speedCar = 150; // 0 to 255
+int speed_low = 60;
+
+ESP8266WebServer server(80);
+Servo Joint1;
+Servo gripper;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  Joint1.attach(16);
+  gripper.attach(14);
+  gripper.write(100);
+  Joint1.write(65);
+
+  // Connecting WiFi
+
+  WiFi.mode(WIFI_AP);           //Only Access point
+  WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  // Starting WEB-server
+  server.on ( "/", HTTP_handleRoot );
+  server.onNotFound ( HTTP_handleRoot );
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+
+  command = server.arg("State");
+  if (command == "F") goForword();
+  else if (command == "B") goBack();
+  else if (command == "L") goLeft();
+  else if (command == "R") goRight();
+  else if (command == "1") speedCar = 51;
+  else if (command == "2") speedCar = 102;
+  else if (command == "3") speedCar = 153;
+  else if (command == "4") speedCar = 204;
+  else if (command == "5") speedCar = 255;
+  else if (command == "S") stopRobot();
+  else if (command == "Q") {
+    Joint1.write(0);
+  }
+  else if (command == "A") {
+    Joint1.write(45);
+  }
+  else if (command == "U") {
+    gripper.write(70);
+  }
+  else if (command == "J") {
+    gripper.write(180);
+  }
+}
+
+void HTTP_handleRoot(void) {
+
+  if ( server.hasArg("State") ) {
+    Serial.println(server.arg("State"));
+  }
+  server.send ( 200, "text/html", "" );
+  delay(1);
+}
+
+void goForword() {
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar);
+}
+
+void goBack() {
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void goRight() {
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, HIGH);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, HIGH);
+  analogWrite(ENB, speedCar);
+}
+
+void goLeft() {
+
+  digitalWrite(IN_1, HIGH);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, HIGH);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
+
+void stopRobot() {
+
+  digitalWrite(IN_1, LOW);
+  digitalWrite(IN_2, LOW);
+  analogWrite(ENA, speedCar);
+
+  digitalWrite(IN_3, LOW);
+  digitalWrite(IN_4, LOW);
+  analogWrite(ENB, speedCar);
+}
